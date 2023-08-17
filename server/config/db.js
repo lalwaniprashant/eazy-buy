1e712f4390221edab48120c01a3e3015cd68d7f3
lalwaniprashant <prashantlalwani55232@gmail.com>
2023-08-17 22:42:35 +0530

    fix backend api bug

--- server/config/db.js
+++ server/config/db.js
@@ -2,9 +2,9 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URL);
-    console.log(`Successfully Conneted To Mongodb Databse`.bgMagenta.white);
+    console.log(`Successfully Conneted To Mongodb Databse`);
   } catch (error) {
-    console.log(`Errro in Mongodb ${error}`.bgRed.white);
+    console.log(`Errro in Mongodb ${error}`);
   }
 };
 
--- server/server.js
+++ server/server.js
@@ -36,5 +36,5 @@ const PORT = process.env.PORT || 8080;
 
 //run listen
 app.listen(PORT, () => {
-  console.log(`Server Running on port ${PORT}`.bgCyan.white);
+  console.log(`Server Running on port ${PORT}`);
 });
